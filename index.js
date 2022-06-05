aad8d7d01182e5c9907f52a919bf511864b04542
trushub-capstone <irvandirizky9@gmail.com>
2022-06-06 02:00:16 +0700

    getdata

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const koneksi = require("./database");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/api/data", (req, res) => {
+  const querySql = "SELECT * FROM tabel_bank";
+
+  koneksi.query(querySql, (err, rows, field) => {
+    if (err) {
+      return res.status(500).json({
+        msg: "ada kesalahan",
+        error: err,
+      });
+    }
+    res.status(200).json({ success: true, data: rows });
+  });
+});
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
