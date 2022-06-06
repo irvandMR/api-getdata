8ed9c2e116613086290174fcaaffb4a5c1e04920
irvandMR <93073265+irvandMR@users.noreply.github.com>
2022-06-06 18:11:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const koneksi = require("./database");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
