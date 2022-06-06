6b2e5596d23c771333126e216d5fb7b4d1fd19f4
trushub-capstone <irvandirizky9@gmail.com>
2022-06-06 17:31:09 +0700

    ganti port dan dep

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const koneksi = require("./database");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
