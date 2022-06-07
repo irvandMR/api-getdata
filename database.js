33aceef54e32e8b7d5dafc89c17d9a68887eafde
trushub-capstone <irvandirizky9@gmail.com>
2022-06-07 16:22:56 +0700

    test

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 
 const koneksi = mysql.createConnection({
+  // connectionLimit: 10,
   host: "34.101.34.5", //"localhost",
   user: "root",
   password: "12345*",
@@ -11,5 +12,20 @@ koneksi.connect((err) => {
   // if (err) throw err;
   console.log("mysql connected");
 });
+
+// koneksi.getConnection(function(err,connection) {
+//   // if (err) throw err;
+//   console.log("mysql connected");
+// });
+
+// pool.getConnection(function (err, connection) {
+//   connection.query("SELECT table_bank FROM banksampah", function (err, rows) {
+//     console.log(pool._freeConnections.indexOf(connection)); // -1
+
+//     connection.release();
+
+//     console.log(pool._freeConnections.indexOf(connection)); // 0
+//   });
+// });
 
 module.exports = koneksi;
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const https = require('https')
+const fs = require("fs")
 const express = require("express");
 const bodyParser = require("body-parser");
 const koneksi = require("./database");
+// const Connection = require("mysql/lib/Connection");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 443;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,6 +23,7 @@ app.get("/api/data", (req, res) => {
     }
     res.status(200).json({ success: true, data: rows });
   });
+  koneksi.end();
 });
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
